fd1c5cb1aa9b5ffd8f6f448fe575c5da99d9a462
hpedeveloper <hpedevelopers@gmail.com>
2022-07-25 18:58:54 +0530

    first commit

---
+++ src/test/java/repository/Launch.java
@@ -0,0 +1,19 @@
+package repository;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Launch {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.co.in/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+	}
+
+}
